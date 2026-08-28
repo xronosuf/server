4a1ccf1181d444917ec512db56deb90bbb140db8
Jason Nowell <jnowell@ufl.edu>
2026-08-28 19:32:09 -0400

    Convert supervising database queries to promises

--- routes/supervising.js
+++ routes/supervising.js
@@ -1,65 +1,123 @@
 var mdb = require('../mdb');
 
 
-exports.isInstructorForLearnerInRepository = function( repositoryName, supposedInstructor, supposedLearner, callback ) {
-    mdb.LtiBridge.find({user: supposedInstructor._id}, function(err, instructorBridges) {
-	if (err) {
-	    callback(err, false);
-	    return;
-	}
-	
-	mdb.LtiBridge.find({user: supposedLearner._id}, function(err, learnerBridges) {
-	    if (err) {
-		callback(err, false);
-		return;
-	    }
+exports.isInstructorForLearnerInRepository = function(
+    repositoryName,
+    supposedInstructor,
+    supposedLearner,
+    callback
+) {
+    Promise.all([
+        mdb.LtiBridge.find({
+            user: supposedInstructor._id
+        }).exec(),
 
-	    callback( null, instructorBridges.some( function(instructorBridge) {
-		// The instructor is actually an instructor of some sort...
-		return instructorBridge.roles.some( function(role) {
-		    return role.match( /Instructor/ ) || role.match( /Administrator/ ) || role.match( /TeachingAssistant/ ) || role.match( /Grader/ );
-		}) &&
-		    // and the learner is actually in that course
-		    learnerBridges.some( function(learnerBridge) {
-			return (instructorBridge.toolConsumerInstanceGuid == learnerBridge.toolConsumerInstanceGuid) &&
-			    (instructorBridge.contextId == learnerBridge.contextId) &&
-			    (instructorBridge.repository == learnerBridge.repository) &&
-			    (instructorBridge.repository == repositoryName);			    
-		    });
-	    }));
-	});
-    });
+        mdb.LtiBridge.find({
+            user: supposedLearner._id
+        }).exec()
+    ])
+        .then(function(results) {
+            var instructorBridges = results[0];
+            var learnerBridges = results[1];
+
+            var good = instructorBridges.some(
+                function(instructorBridge) {
+                    // The instructor is actually an instructor
+                    // of some sort...
+                    return instructorBridge.roles.some(
+                        function(role) {
+                            return (
+                                role.match(/Instructor/) ||
+                                role.match(/Administrator/) ||
+                                role.match(/TeachingAssistant/) ||
+                                role.match(/Grader/)
+                            );
+                        }
+                    ) &&
+                        // and the learner is actually in
+                        // that course
+                        learnerBridges.some(
+                            function(learnerBridge) {
+                                return (
+                                    instructorBridge
+                                        .toolConsumerInstanceGuid ==
+                                        learnerBridge
+                                            .toolConsumerInstanceGuid
+                                ) &&
+                                    (
+                                        instructorBridge.contextId ==
+                                        learnerBridge.contextId
+                                    ) &&
+                                    (
+                                        instructorBridge.repository ==
+                                        learnerBridge.repository
+                                    ) &&
+                                    (
+                                        instructorBridge.repository ==
+                                        repositoryName
+                                    );
+                            }
+                        );
+                }
+            );
+
+            callback(null, good);
+        })
+        .catch(function(err) {
+            callback(err, false);
+        });
 };
+
 
 // Used when we want to view page as another learner
-exports.masquerade = function(req,res,next) {
-    mdb.User.findOne({_id: new mdb.ObjectId(req.params.masqueradingUserId)},
-		     function(err, learner) {
-			 if (err) {
-			     next(err);
-			     return;
-			 }
+exports.masquerade = async function(req, res, next) {
+    var learner;
 
-			 if (!learner) {
-			     next('Could not find user with id ' + req.params.masqueradingUserId);
-			     return;
-			 }
+    try {
+        learner = await mdb.User.findOne({
+            _id: new mdb.ObjectId(
+                req.params.masqueradingUserId
+            )
+        }).exec();
+    } catch (err) {
+        next(err);
+        return;
+    }
 
-			 exports.isInstructorForLearnerInRepository( req.params.repository, req.user, learner,
-								     function(err, good) {
-									 if (err) {
-									     next(err);
-									 } else {
-									     if (!good)
-										 // Should be a different HTTP error code
-										 next('You do not have permission to see the work of that learner.');
-									     else {
-										 req.learner = learner;
-										 next();
-									     }
-									 }
-								     });
-		     });
+    if (!learner) {
+        next(
+            'Could not find user with id ' +
+            req.params.masqueradingUserId
+        );
+        return;
+    }
+
+    /*
+     * This helper intentionally retains its application-level
+     * callback API. Only its internal Mongoose calls were
+     * converted to promises.
+     */
+    exports.isInstructorForLearnerInRepository(
+        req.params.repository,
+        req.user,
+        learner,
+        function(err, good) {
+            if (err) {
+                next(err);
+                return;
+            }
+
+            if (!good) {
+                // Should be a different HTTP error code
+                next(
+                    'You do not have permission to see ' +
+                    'the work of that learner.'
+                );
+                return;
+            }
+
+            req.learner = learner;
+            next();
+        }
+    );
 };
-
-

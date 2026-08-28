3f924822d2d3dcbb4657584b3c5e1bc0e36142f1
Xronos Team <XronosUF@gmail.com>
2026-08-28 15:58:14 -0400

    Use reusable Git CLI helpers in parity probe

--- scripts/modernization/git-cli-read-parity.js
+++ scripts/modernization/git-cli-read-parity.js
@@ -1,118 +1,25 @@
 #!/usr/bin/env node
 'use strict';
 
-var childProcess = require('child_process');
 var fs = require('fs');
 var path = require('path');
+var gitCli = require('./git-cli-read');
 
 var repositoryName = process.argv[2] || 'davidxronosrepo';
 var repositoriesRoot = process.env.XIMERA_REPOSITORIES_ROOT || path.resolve(__dirname, '../../repositories');
-var repositoryPath = path.resolve(repositoriesRoot, repositoryName + '.git');
 
-function git(args, encoding) {
-  return childProcess.execFileSync(
-    'git',
-    ['--git-dir=' + repositoryPath].concat(args),
-    {
-      encoding: encoding === undefined ? 'utf8' : encoding,
-      maxBuffer: 16 * 1024 * 1024
-    }
-  );
-}
+async function main() {
+  var repository = await gitCli.verifyRepository(repositoriesRoot, repositoryName);
 
-function gitText(args) {
-  return git(args, 'utf8').trim();
-}
-
-function verifyRepository() {
-  var stat = fs.statSync(repositoryPath);
-  if (!stat.isDirectory()) {
-    throw new Error('Repository path is not a directory: ' + repositoryPath);
-  }
-
-  if (gitText(['rev-parse', '--is-bare-repository']) !== 'true') {
-    throw new Error('Expected a bare repository: ' + repositoryPath);
-  }
-}
-
-function recentPublishedCommits(branchName, maxCommits) {
-  var sourceShas = gitText([
-    'rev-list',
-    '--topo-order',
-    '--date-order',
-    '-n',
-    String(maxCommits),
-    'refs/heads/' + branchName
-  ]).split(/\n/).filter(Boolean);
-
-  return sourceShas.map(function(sourceSha) {
-    try {
-      var publicationSha = gitText([
-        'rev-parse',
-        '--verify',
-        'refs/tags/publications/' + sourceSha + '^{commit}'
-      ]);
-
-      return {
-        sourceSha: sourceSha,
-        sha: publicationSha
-      };
-    } catch (err) {
-      return null;
-    }
-  }).filter(Boolean);
-}
-
-function treeEntry(commitSha, pathname) {
-  var output;
-
-  try {
-    output = gitText(['ls-tree', commitSha, '--', pathname]);
-  } catch (err) {
-    return null;
-  }
-
-  if (!output) {
-    return null;
-  }
-
-  var tab = output.indexOf('\t');
-  if (tab < 0) {
-    throw new Error('Unexpected ls-tree output: ' + output);
-  }
-
-  var header = output.slice(0, tab).split(/\s+/);
-  var entryPath = output.slice(tab + 1);
-
-  return {
-    mode: header[0],
-    type: header[1],
-    sha: header[2],
-    path: entryPath
-  };
-}
-
-function readBlob(blobHash) {
-  var type = gitText(['cat-file', '-t', blobHash]);
-  if (type !== 'blob') {
-    throw new Error('Object is not a blob: ' + blobHash + ' (' + type + ')');
-  }
-
-  return git(['cat-file', 'blob', blobHash], null);
-}
-
-function main() {
-  verifyRepository();
-
-  var published = recentPublishedCommits('master', 100);
+  var published = await gitCli.recentPublishedCommits(repository, 'master', 100);
   if (!published.length) {
     throw new Error('No published commits found on master.');
   }
 
   var first = published[0];
   var htmlPath = 'aFirstFolder/aFirstActivity.html';
-  var html = treeEntry(first.sha, htmlPath);
-  var metadata = treeEntry(first.sha, 'metadata.json');
+  var html = await gitCli.treeEntry(repository, first.sha, htmlPath);
+  var metadata = await gitCli.treeEntry(repository, first.sha, 'metadata.json');
 
   if (!html || html.type !== 'blob') {
     throw new Error('Expected HTML blob not found: ' + htmlPath);
@@ -122,7 +29,7 @@ function main() {
     throw new Error('Expected metadata.json blob not found.');
   }
 
-  var blob = readBlob(html.sha);
+  var blob = await gitCli.readBlob(repository, html.sha);
 
   console.log('Repository:', repositoryName);
   console.log('Bare repository: yes');
@@ -159,9 +66,7 @@ function main() {
   console.log('GIT CLI READ PARITY PASSED');
 }
 
-try {
-  main();
-} catch (err) {
+main().catch(function(err) {
   console.error(err && err.stack ? err.stack : err);
   process.exit(1);
-}
+});

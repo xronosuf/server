3de167835eb4a663ba1a20c6e2d37ba0a7eefb52
Xronos Team <XronosUF@gmail.com>
2026-09-01 16:19:19 -0400

    Test escaped ampersands in published scripts

--- test/activity-html-compat.test.js
+++ test/activity-html-compat.test.js
@@ -21,8 +21,26 @@ describe('activity HTML compatibility', function() {
         assert.ok(fixed.indexOf('&gt; 0') === -1);
     });
 
+    it('decodes escaped ampersands inside script bodies', function() {
+        var html = [
+            '<script type="text/javascript">',
+            'return (',
+            "a.derivative('a').equals(b.derivative('a')) &amp;&amp;",
+            "a.derivative('b').equals(b.derivative('b')) &amp;&amp;",
+            "a.derivative('c').equals(b.derivative('c'))",
+            ');',
+            '</script>'
+        ].join('\n');
+
+        var fixed = fixActivityHTML(html);
+
+        assert.ok(fixed.indexOf("equals(b.derivative('a')) &&") !== -1);
+        assert.ok(fixed.indexOf("equals(b.derivative('b')) &&") !== -1);
+        assert.ok(fixed.indexOf('&amp;&amp;') === -1);
+    });
+
     it('leaves named entities in ordinary prose untouched', function() {
-        var html = '<p>Use x &lt; 5 and y &gt; 2.</p>';
+        var html = '<p>Use x &lt; 5, y &gt; 2, and A &amp; B.</p>';
 
         assert.strictEqual(fixActivityHTML(html), html);
     });
@@ -38,15 +56,15 @@ describe('activity HTML compatibility', function() {
 
     it('handles several script blocks without changing text between them', function() {
         var html = [
-            '<script>if (a &lt; b) { first(); }</script>',
-            '<p>a &lt; b</p>',
+            '<script>if (a &lt; b &amp;&amp; ready) { first(); }</script>',
+            '<p>a &lt; b &amp;&amp; ready</p>',
             '<script>if (c &gt; d) { second(); }</script>'
         ].join('\n');
 
         var fixed = fixActivityHTML(html);
 
-        assert.ok(fixed.indexOf('if (a < b)') !== -1);
-        assert.ok(fixed.indexOf('<p>a &lt; b</p>') !== -1);
+        assert.ok(fixed.indexOf('if (a < b && ready)') !== -1);
+        assert.ok(fixed.indexOf('<p>a &lt; b &amp;&amp; ready</p>') !== -1);
         assert.ok(fixed.indexOf('if (c > d)') !== -1);
     });
 

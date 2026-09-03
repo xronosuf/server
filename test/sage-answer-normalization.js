9bac3a07b507022abc38b5c1dba069ebd3645fdb
Xronos Team <XronosUF@gmail.com>
2026-09-03 18:00:00 -0400

    Test Sage macro detection in string answers

--- test/sage-answer-normalization.js
+++ test/sage-answer-normalization.js
@@ -2,6 +2,29 @@ var assert = require('assert');
 var normalization = require('../public/javascripts/sage-answer-normalization');
 
 describe('Sage canonical answer string normalization', function() {
+    it('detects authored Sage macros inside string answers', function() {
+        assert.strictEqual(
+            normalization.containsSageMacro('\\sage{answerValue}'),
+            true
+        );
+    });
+
+    it('detects stable-id Sage macros inside string answers', function() {
+        assert.strictEqual(
+            normalization.containsSageMacro(
+                '\\xronosSageById{stable-id}{answerValue}'
+            ),
+            true
+        );
+    });
+
+    it('does not classify literal string answers as Sage', function() {
+        assert.strictEqual(
+            normalization.containsSageMacro('No solution'),
+            false
+        );
+    });
+
     it('extracts DNE from Sage string display LaTeX', function() {
         assert.strictEqual(
             normalization.extractSageString('\\text{\\texttt{DNE}}'),

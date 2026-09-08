94427590f08d9e365100bb9bcd0ff59a38537e7b
Xronos Team <XronosUF@gmail.com>
2026-09-08 15:31:37 -0400

    Version MathJax dynamic asset root

--- public/javascripts/mathjax.js
+++ public/javascripts/mathjax.js
@@ -1,3 +1,10 @@
+var applicationVersionPath = require('./application-version-path');
+var fallbackVersion = require('../../dbm.json').version;
+var applicationVersion = applicationVersionPath.pageApplicationVersion(
+    document,
+    fallbackVersion
+);
+
 window.MathJax = {
     delayStartupUntil : "configured",
 
@@ -35,7 +42,12 @@ window.MathJax = {
 	}
     },
 
-    root: window.toValidPath("/node_modules/mathjax/")
+    root: window.toValidPath(
+        applicationVersionPath.versionedNodeModulesPath(
+            applicationVersion,
+            'mathjax/'
+        )
+    )
 };
 require('mathjax2');
 
